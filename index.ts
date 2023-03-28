42e1f4d93c71e5607a0c255342bae5252260be20
Wojtek <rossa21@gmail.com>
2023-03-28 22:51:24 +0200

    install express, cors, express-errors, rate-limiter and types

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+
+app.listen(3001, 'localhost', () => {
+    console.log('Listening on port 3001. http://localhost:3001');
+})
