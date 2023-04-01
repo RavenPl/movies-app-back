3e2289f25ec317703b21b1a34a4c9a3fc440e7e6
Wojtek <rossa21@gmail.com>
2023-04-01 21:21:14 +0200

    Install jsonwebtoken and create function to use it.

--- index.ts
+++ index.ts
@@ -1,35 +1,22 @@
 import express, {json} from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 
 import {handleErrors} from "./utils/errors";
 import {config} from "./config/config";
-import './utils/db';
-import {UserRecord} from "./records/user.record";
+import {AuthRouter} from './routers/auth'
 
 const app = express();
-
-app.get('/', (req, res) => {
-
-    res.json({msg: 'ok'})
-})
 
 app.use(cors({
     origin: config.corsOrigin,
 }));
 app.use(json());
+
+app.use('/auth', AuthRouter);
+
 app.use(handleErrors);
 
 app.listen(3001, 'localhost', () => {
     console.log('Listening on port 3001. http://localhost:3001');
 });
-
-(async () => {
-    const obj = {
-        email: "AA@bB.com ",
-        password: "123456"
-    };
-
-    const newUser = await new UserRecord(obj);
-    await newUser.insert();
-
-})()
---
+++ utils/tokenCreator.ts
@@ -0,0 +1,30 @@
+import {v4 as uuid} from 'uuid';
+import {sign} from "jsonwebtoken";
+
+import {UserRecord} from "../records/user.record";
+import {config} from "../config/config";
+
+export const createToken = async (currentTokenId: string) => {
+
+    const payload = {id: currentTokenId};
+    const accessToken = sign(payload, config.jwtPassword, {
+        expiresIn: "6h",
+    });
+    return {
+        accessToken,
+    };
+};
+
+export const generateToken = async (user: UserRecord) => {
+    let token;
+    let userWithThisToken = null;
+    do {
+        token = uuid();
+        userWithThisToken = await UserRecord.getOneByToken(token);
+    } while (userWithThisToken);
+
+    user.currentTokenId = token;
+    await user.save();
+
+    return token;
+};
