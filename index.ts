9a2295c5709632646e02f380f3904c2c1da5adb3
Wojtek <rossa21@gmail.com>
2023-04-12 17:46:45 +0200

    Added new routes into index.ts.

--- index.ts
+++ index.ts
@@ -4,12 +4,15 @@ import cors from 'cors';
 import cookieParser from "cookie-parser";
 
 import {handleErrors} from "./utils/errors";
+import {notFound} from "./middlewares/notFound";
 import {config} from "./config/config";
 import {AuthRouter} from './routers/auth'
-import {notFound} from "./middlewares/notFound";
+import {UserRouter} from "./routers/user";
+import {BookmarksRouter} from "./routers/bookmarks";
 
 const router = Router();
 const app = express();
+
 app.use(cors({
     origin: config.corsOrigin,
     credentials: true,
@@ -20,6 +23,9 @@ app.use(cookieParser());
 
 app.use('/movies', router);
 router.use('/auth', AuthRouter);
+router.use('/user', UserRouter);
+router.use('/user/bookmarks', BookmarksRouter);
+
 app.use('*', notFound);
 
 app.use(handleErrors);
