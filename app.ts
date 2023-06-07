813b74658c2d20c5e6933512414d63b31a35a591
Wojtek <rossa21@gmail.com>
2023-06-07 18:38:56 +0200

    Change the index.ts file name and bumped app version.

--- app.ts
+++ app.ts
@@ -1,17 +1,19 @@
 import express, {json, Router} from 'express';
 import 'express-async-errors';
+import rateLimit from "express-rate-limit";
 import cors from 'cors';
 import cookieParser from "cookie-parser";
-import rateLimit from "express-rate-limit";
 import helmet from 'helmet';
 
-import {handleErrors} from "./utils/errors";
-import {notFound} from "./middlewares/notFound";
 import {config} from "./config/config";
 import {AuthRouter} from './routers/auth'
 import {UserRouter} from "./routers/user";
 import {BookmarksRouter} from "./routers/bookmarks";
+import {notFound} from "./middlewares/notFound";
+import {handleErrors} from "./utils/errors";
+import {configDotenv} from "dotenv";
 
+configDotenv();
 const router = Router();
 const app = express();
 
@@ -28,8 +30,8 @@ app.use('/movies', router);
 router.use('/auth', AuthRouter);
 router.use('/user', UserRouter);
 router.use('/user/bookmarks', BookmarksRouter);
-
 app.use('*', notFound);
+
 app.use(rateLimit({
     windowMs: 5 * 60 * 1000,
     max: 100,
